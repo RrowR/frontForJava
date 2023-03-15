d61d92c32e5b3084bf420ae9f12a56c1873867bb
Memo <2415782224@qq.com>
2023-03-16 03:09:30 +0800

    vue项目的组件之间的调用,如何编写试图代码,修改main.js默认调用的App.vue的代码.数据绑定等

---
+++ front/myvue2/client/src/views/Demo1Vue.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <h1>{{ age > 18 ? '成年' : '未成年' }}</h1>
+    </div>
+</template>
+
+<script>
+const options = {
+    data: function () {
+        return {
+            name: '猫羽雫',
+            age: 19
+        };
+    }
+};
+export default options;
+</script>
