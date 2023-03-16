c4e9fe0878da0b19e525af86d421b0c0a2fd9641
Memo <2415782224@qq.com>
2023-03-16 15:54:11 +0800

    :value 数据绑定

---
+++ front/myvue2/client/src/views/Demo2Vue.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input type="text" v-bind:value="age">
+        <input type="date" v-bind:value="birthday">
+        <!-- :value简化的数据绑定 -->
+        <input type="text" :value="name">
+    </div>
+</template>
+
+<script>
+const options = {
+    data: function () {
+        return {
+            name: '猫羽雫', age: 19, birthday: '1999-01-01'
+        };
+    }
+};
+export default options;
+</script>
