a0ab3888e52c2565b797f35a05912794a500513a
Memo <2415782224@qq.com>
2023-03-19 20:07:48 +0800

    element使用按钮组件

---
+++ front/myvue2/client/src/views/013Element按钮的使用.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+        <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </div>
+</template>
+<script>
+import axios from '../utils/myaxios';
+const options = {
+    async mounted() {
+        // 编写一下axios的使用
+        const resp = await axios.get("/api/student01");
+        console.log(resp.data.data);
+    },
+}
+export default options;
+</script>
