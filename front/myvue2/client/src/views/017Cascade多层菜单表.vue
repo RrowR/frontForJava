778846b145a4be5481d193e2ab9ee79f323689d6
RrowR <2415782224@qq.com>
2023-03-31 00:34:46 +0800

    前端数据处理

--- front/myvue2/client/src/views/017Cascade多层菜单表.vue
+++ front/myvue2/client/src/views/017Cascade多层菜单表.vue
@@ -4,37 +4,36 @@
 </template>
   
 <script>
+import axios from 'axios';
 export default {
-    mounted() {
+    async mounted() {
+        let resp = await axios.get('/api/menu');
+        const array = resp.data;
+        const map = new Map();
 
+        for (const { id, name, pid } of array) {
+            map.set(id, { value: id, label: name, pid: pid })
+        }
+
+        const top = [];
+        for (const obj of map.values()) {
+            // 获取根节点
+            const parent = map.get(obj.pid);
+            if (parent !== undefined) {
+                parent.children ??= [];
+                parent.children.push(obj);
+            } else {
+                // 最顶层节点
+                top.push(obj);
+            }
+        }
+        console.log(top);
+        this.options = top;
     },
     data() {
         return {
             options: [
-                {
-                    value: 100, label: '主页', children: [
-                        {
-                            value: 101, label: '菜单1', children: [
-                                { value: 105, label: '子项1' },
-                                { value: 106, label: '子项2' }
-                            ]
-                        },
-                        {
-                            value: 102, label: '菜单2', children: [
-                                { value: 107, label: '子项3' },
-                                { value: 108, label: '子项4' },
-                                { value: 109, label: '子项5' }
-                            ]
-                        },
-                        {
-                            value: 103, label: '菜单3', children: [
-                                { value: 110, label: '子项6' },
-                                { value: 111, label: '子项7' }
-                            ]
-                        },
-                        { value: 104, label: '菜单4' }
-                    ]
-                }
+
             ]
         }
     },
