f03235e860a4cf9211303bb052df0d6fefa4e232
Memo <2415782224@qq.com>
2023-05-14 03:54:40 +0800

    vuex

--- front/myvue2/client/src/main.js
+++ front/myvue2/client/src/main.js
@@ -2,14 +2,15 @@ import Vue from 'vue'
 // import App from './App.vue'
 // 以这个页面作为基础页面
 // import e1 from './views/019Router嵌套.vue'
-import e1 from './views/022DynamicRouterPage.vue'
+// import e1 from './views/022DynamicRouterPage.vue'
+import e1 from './views/023Vuex.vue'
 // import e1 from './views/020页面布局.vue'
 // import router from './router'
 // import router from './router/myrouter'
 // import router from './router/dynamicRouter'
 // import router from './router/020layout'
 // import router from './router/021navigationMenu'
-import router from './router/022dynamicRouter'
+import router from './router/023VueX'
 import store from './store'
 // elementui导入
 import Element from 'element-ui';
