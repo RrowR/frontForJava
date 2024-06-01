87f33cbe91d46e40596904be2247fd05e904ac62
Memo <2415782224@qq.com>
2024-06-01 23:37:13 +0800

    作用域&重复说明

---
+++ guiguFront/frontQuickStart/demo-es6/lib/core.js
@@ -0,0 +1,24 @@
+// 1、let  规范作用域
+// var a = 1;
+// let b = 1;
+
+
+// {
+//     var a = 1;
+//     let b = 1;
+// }
+
+// console.log(a);
+// console.log(b);
+
+// ==========    1.2  重复声明
+// var a = 1;
+// var a = 2;
+// var a = 3;
+// var a = 4;
+// console.log(a);
+// let b = 1;
+// console.log(b);
+// const c = 1;
+// c = 2;
+// console.log(c);  //Assignment to constant variable.
